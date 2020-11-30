d51783990efe070bcccf1302701115bab2399da4
Brandon Tang <btang@sdsu.edu>
2020-11-29 23:20:21 -0800

    added codes folder and BGMvolumeslider and SFXVolumeslider codes

---
+++ Assets/Codes/BGMVolumeSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class BGMVolumeSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("bgVol", 1f);
+    }
+
+    public void SetLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("bgVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("bgVol", sliderValue);
+    }
+}
---
+++ Assets/Codes/SFXVolumeSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SFXVolumeSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("sfxVol", 1f);
+    }
+
+    public void SetLevel()
+    {
+        float sliderValue = slider.value;
+        mixer.SetFloat("sfxVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("sfxVol", sliderValue);
+    }
+}
