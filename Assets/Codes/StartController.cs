4b10a563f2f87e7dacaf4f23094d4206bd0d5218
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2020-12-01 21:12:52 -0800

    Added back button with scene management script

---
+++ Assets/Codes/StartController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    public void LoadSceneByNumber(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("quit called");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else  
+        Application.Quit();
+#endif
+    }
+}
