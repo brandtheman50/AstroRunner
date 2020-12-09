a9207bff1b8300f38346a3ef681dac2a71e72f9e
James Thomas <jethomas@sdsu.edu>
2020-12-08 20:41:50 -0800

    Adding CamMove Script

---
+++ Assets/Codes/CamMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 40);
+
+    }
+
+    void Update()
+    {
+
+    }
+}
