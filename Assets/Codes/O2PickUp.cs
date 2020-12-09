6ca6cd4f39e9b3543fe9432e6696c64c60d61d49
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2020-12-09 10:33:23 -0800

    Modifying oxygen script

---
+++ Assets/Codes/O2PickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class O2PickUp : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 5, 0);
+    }
+
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "astro")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
