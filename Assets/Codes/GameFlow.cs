73333e7aff45bade1d2347a7cad506e1fec29d8e
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2020-12-08 20:42:38 -0800

    Scripts for endless course and camera movement added

---
+++ Assets/Codes/GameFlow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+    public Transform tileObj;
+    private Vector3 nextTileSpawn;
+
+    void Start()
+    {
+        nextTileSpawn.z = 50;
+        StartCoroutine(spawnTile());
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator spawnTile()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(tileObj, nextTileSpawn, tileObj.rotation);
+        nextTileSpawn.z += 100;
+        StartCoroutine(spawnTile());
+    }
+}
