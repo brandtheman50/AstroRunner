a45dde28790512e28484b9c198f6230afacaedbd
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2020-12-09 02:04:50 -0800

    Added collider to player

--- Assets/Artwork/Stylized Astronaut/Character/Player.cs
+++ Assets/Artwork/Stylized Astronaut/Character/Player.cs
@@ -5,29 +5,40 @@ public class Player : MonoBehaviour {
 
 	private float speed = 3f;
 	private Vector3 moveDir;
+	private string midJump = "n";
 
 		void Start () {
 			
 			GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 20);
-			StartCoroutine(stopXMovement());
+			
+			
 
 		}
 
 		void Update () {
 		moveDir.x = Input.GetAxisRaw("Horizontal");
-		if (Input.GetKey("a"))
+		
+		if (Input.GetKey("a") && (transform.position.x > -2.7))
         {
 			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + moveDir * speed * Time.fixedDeltaTime);
 			StartCoroutine(stopXMovement());
 		}
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") && (transform.position.x < 3.5))
         {
 			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + moveDir *speed * Time.fixedDeltaTime);
 			StartCoroutine(stopXMovement());
 		}
+        
 
 		}
-
+	
+	private void OnTriggerEnter(Collider other)
+    {
+		if(other.tag == "obstacle")
+        {
+			Debug.Log("Hiit");
+        }
+    }
 	IEnumerator stopXMovement()
     {
 		yield return new WaitForSeconds(1);
