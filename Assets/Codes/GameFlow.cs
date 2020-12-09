9bc7264da9e428eab5fc5d46b6d904c7fda67485
Brandon Altamirano <baltamiranoortega@sdsu.edu>
2020-12-08 23:34:51 -0800

    Modified GameFlow script to spawn random positions of fence obstacle

--- Assets/Codes/GameFlow.cs
+++ Assets/Codes/GameFlow.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class GameFlow : MonoBehaviour
 {
     public Transform tileObj;
-   
-
     private Vector3 nextTileSpawn;
+    public Transform fence;
+    private Vector3 nextFence;
+    private int randx;
 
     void Start()
     {
@@ -25,7 +26,12 @@ public class GameFlow : MonoBehaviour
     IEnumerator spawnTile()
     {
         yield return new WaitForSeconds(1);
+        randx = Random.Range(-2,4);
+        nextFence = nextTileSpawn;
+        nextFence.x = randx;
+        nextFence.y = 0;
         Instantiate(tileObj, nextTileSpawn, tileObj.rotation);
+        Instantiate(fence, nextFence, fence.rotation);
         nextTileSpawn.z += 50;
         StartCoroutine(spawnTile());
     }
