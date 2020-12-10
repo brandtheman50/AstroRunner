db182375104bb6dfd975e32da6601cb60682cc30
Dominik Skulec <dskulec@sdsu.edu>
2020-12-10 13:15:30 -0800

    Credits Scene finished, text is really blurry for some reason but I'm too lazy to fix now

---
+++ Assets/Codes/Scroller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroller : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (true)
+        {
+            Vector3 current = gameObject.transform.position;
+
+            Vector3 increment =
+             new Vector3(current.x,
+                         current.y + .4f * Mathf.Sin(Mathf.Deg2Rad * 55),
+                         current.z + .4f * Mathf.Cos(Mathf.Deg2Rad * 55));
+
+            gameObject.transform.position = increment;
+        }
+    }
+}
