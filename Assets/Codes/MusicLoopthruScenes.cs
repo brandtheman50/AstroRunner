124f5d8d58ad516cc1d32b3865466dd2c97b5dbb
Brandon Tang <btang@sdsu.edu>
2020-12-09 23:08:44 -0800

    add code to loop music through scenes

---
+++ Assets/Codes/MusicLoopthruScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicLoopthruScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
