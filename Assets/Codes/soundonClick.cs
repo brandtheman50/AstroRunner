11dddf9123b112a9dad4c7852cbf31edeb019f90
Brandon Tang <btang@sdsu.edu>
2020-12-10 00:20:06 -0800

    added soundonclick code to folder

---
+++ Assets/Codes/soundonClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundonClick : MonoBehaviour
+{
+    void Update()
+    {
+        // If the left mouse button is pressed down...
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        // If the left mouse button is released...
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
