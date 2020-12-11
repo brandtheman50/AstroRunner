9dca6f8b03c349532bc4a8c5f3d13ce02698d21d
James Thomas <jethomas@sdsu.edu>
2020-12-10 16:34:48 -0800

    Updated EndScene and include scripts to allow use of button/to display information from game

---
+++ Assets/Codes/EndSceneStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndSceneStats : MonoBehaviour
+{
+    [SerializeField]
+    Text YourScoreText;
+    [SerializeField]
+    Text HighScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        YourScoreText.text = "Your Score: " + GameFlow.yourScore;
+        HighScoreText.text = "High Score: " + GameFlow.highScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
