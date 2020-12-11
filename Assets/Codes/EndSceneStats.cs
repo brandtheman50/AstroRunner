3578adbfab254d1426c608bb48e72276623c3231
James Thomas <jethomas@sdsu.edu>
2020-12-10 16:50:31 -0800

    Updated EndScene to display coins collected and updated buttons

--- Assets/Codes/EndSceneStats.cs
+++ Assets/Codes/EndSceneStats.cs
@@ -9,12 +9,15 @@ public class EndSceneStats : MonoBehaviour
     Text YourScoreText;
     [SerializeField]
     Text HighScoreText;
+    [SerializeField]
+    Text CoinCollectedText;
 
     // Start is called before the first frame update
     void Start()
     {
         YourScoreText.text = "Your Score: " + GameFlow.yourScore;
         HighScoreText.text = "High Score: " + GameFlow.highScore;
+        CoinCollectedText.text = "Coins Collected: " + GameFlow.totalCoins;
     }
 
     // Update is called once per frame
